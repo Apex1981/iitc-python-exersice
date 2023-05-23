2bdc3d3e3be63981d1257539cb85c06786cf1c49
Apex1981 <apex151013@gmail.com>
2023-05-23 07:26:29 +0000

    exercise-2

--- python-1-exercise.py
+++ python-1-exercise.py
@@ -1,7 +1,18 @@
-def print_integer_divisors(number):
-    divisors = [i for i in range(1, number + 1) if number % i == 0]
-    print(divisors)
 
-number = int(input("Enter a number: "))
-print("Integer divisors:")
-print_integer_divisors(number)
+numbers = []
+sum_of_numbers = 0
+count = 1
+
+while True:
+    number = int(input("Please enter number #{}: ".format(count)))
+    if number < 0:
+        break
+
+    numbers.append(number)
+    sum_of_numbers += number
+    average = sum_of_numbers / count
+
+    print("Please enter number #{} (avg={:.2f}. Sum={})".format(count, average, sum_of_numbers))
+    count += 1
+
+print("Thank you. Goodbye.")
